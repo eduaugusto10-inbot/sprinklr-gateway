1adf28af60f97b3924b8b7d14fbd1106f1147b97
Eduardo Augusto Gomes de Oliveira <eduardo.oliveira@inbot.com.br>
2025-05-15 21:17:51 -0300

    Atualizada a versão para 1-034 e substituídos os IDs de cliente nas consultas do SprinklrCredentialsDAO.

--- src/models/SprinklrCredentialsDAO.js
+++ src/models/SprinklrCredentialsDAO.js
@@ -5,7 +5,7 @@ class SprinklrCredentialsDAO {
     return new Promise((resolve, reject) => {
       db.query(
         "SELECT * FROM sprinklr_credentials WHERE client_id = ? ORDER BY id DESC LIMIT 1;",
-        ["265cez298wgp99eahsrk46cq"],
+        ["sbkfQSxyuyo792YwbMdBFcLNwkUDGJL7d1UmxwohaopXlPaB"],
         (err, result) => {
           if (err) {
             reject(err);
@@ -20,7 +20,7 @@ class SprinklrCredentialsDAO {
     return new Promise((resolve, reject) => {
       db.query(
         "SELECT * FROM sprinklr_credentials WHERE client_id = ? ORDER BY id DESC LIMIT 1;",
-        ["265cez298wgp99eahsrk46cq"],
+        ["sbkfQSxyuyo792YwbMdBFcLNwkUDGJL7d1UmxwohaopXlPaB"],
         (err, result) => {
           if (err) {
             reject(err);
